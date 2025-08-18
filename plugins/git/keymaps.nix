1a205282b7337727627af30d3f7fe53a439baa07
agdral <ezequiel.escobar@proton.me>
2025-08-18 05:09:13 -0300

    feat(git): remove hunk-related keymaps from gitsigns configuration

--- plugins/git/keymaps.nix
+++ plugins/git/keymaps.nix
@@ -29,35 +29,5 @@
       action = "<cmd>lua require('gitsigns').blame_line()<CR>";
       options.desc = "Git Blame";
     }
-    {
-      mode = "n";
-      key = "<leader>ghv";
-      action = "<cmd>lua require('gitsigns').select_hunk()<CR>";
-      options.desc = "Git Hunk Visual Select";
-    }
-    {
-      mode = "n";
-      key = "<leader>ghp";
-      action = "<cmd>lua require('gitsigns').preview_hunk()<CR>";
-      options.desc = "Git Hunk Preview";
-    }
-    {
-      mode = "n";
-      key = "<leader>ghr";
-      action = "<cmd>lua require('gitsigns').reset_hunk()<CR>";
-      options.desc = "Git Hunk Reset";
-    }
-    {
-      mode = "n";
-      key = "<leader>ghs";
-      action = "<cmd>lua require('gitsigns').stage_hunk()<CR>";
-      options.desc = "Git Hunk Stage";
-    }
-    {
-      mode = "n";
-      key = "<leader>ghu";
-      action = "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>";
-      options.desc = "Git Hunk Undo Stage";
-    }
   ];
 }
