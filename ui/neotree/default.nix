d1f336ad796875eb7d4389f71f0e70898c11b42b
agdral <ezequiel.escobar@proton.me>
2025-10-15 00:32:18 -0300

    fix neo-tree settings

--- ui/neotree/default.nix
+++ ui/neotree/default.nix
@@ -5,41 +5,43 @@
 
   plugins.neo-tree = {
     enable = true;
-    enableDiagnostics = true;
-    enableGitStatus = true;
-    enableModifiedMarkers = true;
-    enableRefreshOnWrite = true;
-    hideRootNode = true;
-    closeIfLastWindow = true;
-    popupBorderStyle = "rounded";
-    buffers = {
-      bindToCwd = false;
-      followCurrentFile = {
-        enabled = true;
+    settings = {
+      enableDiagnostics = true;
+      enableGitStatus = true;
+      enableModifiedMarkers = true;
+      enableRefreshOnWrite = true;
+      hideRootNode = true;
+      closeIfLastWindow = true;
+      popupBorderStyle = "rounded";
+      buffers = {
+        bindToCwd = false;
+        followCurrentFile = {
+          enabled = true;
+        };
       };
-    };
-    window = {
-      width = 25;
-      height = 15;
-      autoExpandWidth = false;
-      mappings = {
-        "<space>" = "none";
+      window = {
+        width = 25;
+        height = 15;
+        autoExpandWidth = false;
+        mappings = {
+          "<space>" = "none";
+        };
       };
-    };
-    filesystem = {
-      hijackNetrwBehavior = "open_default";
-      filteredItems = {
-        hideDotfiles = true;
-        alwaysShow = [
-          ".gitignore"
-          ".env"
-          ".env-example"
-          ".ccls"
-          ".justfile"
-          ".envrc"
-          ".pio"
-        ];
-        showHiddenCount = false;
+      filesystem = {
+        hijackNetrwBehavior = "open_default";
+        filteredItems = {
+          hideDotfiles = true;
+          alwaysShow = [
+            ".gitignore"
+            ".env"
+            ".env-example"
+            ".ccls"
+            ".justfile"
+            ".envrc"
+            ".pio"
+          ];
+          showHiddenCount = false;
+        };
       };
     };
   };
