0acc354987cad77cb8514a31357bfef4c11f1f0c
agdral <ezequiel.escobar@proton.me>
2025-12-12 22:03:48 -0300

    remove luaconfig neotree

--- ui/neotree/default.nix
+++ ui/neotree/default.nix
@@ -46,24 +46,5 @@
         };
       };
     };
-
-    luaConfig = ''
-      event_handlers = {
-            {
-              event = "neo_tree_buffer_enter",
-              handler = function()
-                -- This effectively hides the cursor
-                vim.cmd 'highlight! Cursor blend=100'
-              end
-            },
-            {
-              event = "neo_tree_buffer_leave",
-              handler = function()
-                -- Make this whatever your current Cursor highlight group is.
-                vim.cmd 'highlight! Cursor guibg=#5f87af blend=0'
-              end
-            }
-          },
-    '';
   };
 }
