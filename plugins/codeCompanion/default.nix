a93157d51655d7f805c2b887366b5f4674ec748f
agdral <ezequiel.escobar@proton.me>
2026-01-29 14:58:06 -0300

    fix codecompanion

--- plugins/codeCompanion/default.nix
+++ plugins/codeCompanion/default.nix
@@ -7,26 +7,12 @@
 with lib; let
   cfg = config.nvim_plugins;
   keymapsF = import ./keymaps.nix;
-
-  codecompanion-new = pkgs.vimUtils.buildVimPlugin {
-    pname = "codecompanion.nvim";
-    version = "18.5.1-unstable-2026-01-24";
-    src = pkgs.fetchFromGitHub {
-      owner = "olimorris";
-      repo = "codecompanion.nvim";
-      rev = "26fa57cd0ac019c9bae539e7530f7c9401cbee55";
-      hash = "sha256-NEgSYj+ja0EQTwPGur8D7PcECSdZ85z+w61vf1ES14s=";
-    };
-    meta.homepage = "https://github.com/olimorris/codecompanion.nvim/";
-  };
-
 in {
   config = mkIf cfg.codeCompanion {
     keymaps = keymapsF;
 
     plugins.codecompanion = {
       enable = true;
-      package = codecompanion-new;
       settings = {
         http.adapters = {
           deepseek = {
