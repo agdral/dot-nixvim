1415bc7835971679bfa6e68ff691af04c7a31c1a
agdral <ezequiel.escobar@proton.me>
2026-02-20 19:27:41 -0300

    add module nixvim

--- flake.nix
+++ flake.nix
@@ -3,17 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     import-tree.url = "github:vic/import-tree";
   };
 
   outputs = {
     nixpkgs,
     import-tree,
+    nixvim,
     ...
   }: let
     lib = nixpkgs.lib;
   in {
-    nixosModules.general = ./general;
+    nixosModules.general = {
+      imports = [
+        nixvim.nixosModules.nixvim
+        ./general
+      ];
+    };
     nixosModules.lsp = ./lsp;
     nixosModules.plugins = import-tree.filter (lib.hasSuffix "/default.nix") ./plugins;
     nixosModules.ui = import-tree.filter (lib.hasSuffix "/default.nix") ./ui;
