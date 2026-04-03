7c60229fdc3dca4df41c42f25b9dead3b43bd17a
agdral <ezequiel.escobar@proton.me>
2026-04-03 07:31:07 -0300

    remove prettier

--- lsp/lang/javascript/default.nix
+++ lsp/lang/javascript/default.nix
@@ -1,17 +1,9 @@
-{pkgs, ...}: {
+{
   plugins = {
     lsp.servers = {
       biome = {
         enable = true;
         filetypes = ["javascript" "javascriptreact" "typescript" "typescript.tsx" "typescriptreact" "graphql"];
-      };
-    };
-
-    none-ls.sources.formatting = {
-      prettier = {
-        enable = true;
-        packages = pkgs.prettier;
-        disableTsServerFormatter = false;
       };
     };
   };
