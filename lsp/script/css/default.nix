46ceba989f1d16d8bc06d4fa32f918a4f3395b57
agdral <ezequiel.escobar@proton.me>
2026-05-21 15:52:14 -0300

    add config for tailwindcss

--- lsp/script/css/default.nix
+++ lsp/script/css/default.nix
@@ -1,8 +1,33 @@
 {
-  lsp.servers = {
+  plugins.lsp.servers.tailwindcss = {
     # se debe instalar en el proyecto: npm install @tailwindcss/language-server
-    tailwind.enable = true; 
+    enable = true;
+
+    filetypes = [
+      "html"
+      "css"
+      "scss"
+      "javascript"
+      "javascriptreact"
+      "typescript"
+      "typescriptreact"
+      "svelte"
+      "vue"
+      "astro"
+      "nunjucks"
+      "twig"
+    ];
+
+    settings = {
+      tailwindCSS = {
+        # Para proyectos con configs no estándar
+        experimental = {
+          configFile = "./src/app/globals.css";
+        };
+      };
+    };
   };
+
   plugins = {
     none-ls.sources.formatting = {
       rustywind.enable = true;
